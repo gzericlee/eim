bc439d14d145e3112fe335d2f9d633c76085f5c9
EricLee <lirui@bingosoft.net>
2022-05-26 22:45:27 +0800

    微调

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 BRANCH=$(git symbolic-ref --short -q HEAD)
 COMMIT=$(git rev-parse --verify HEAD)
 NOW=$(date '+%Y-%m-%dT%H:%M:%S')
 
+echo "Branch: ${BRANCH}，Commit: ${COMMIT}，Date: ${NOW}"
+
+echo "Compiling eim_gateway service..."
 go build -o dist/eim_gateway -tags netgo -ldflags \
 "-s -w
 -X eim/build.ServiceName=EIM-Gateway
@@ -11,6 +15,7 @@ go build -o dist/eim_gateway -tags netgo -ldflags \
 -X eim/build.Date=${NOW}" \
 ./cmd/gateway
 
+echo "Compiling eim_storage service..."
 go build -o dist/eim_storage -tags netgo -ldflags \
 "-s -w
 -X eim/build.ServiceName=EIM-Storage
@@ -19,6 +24,7 @@ go build -o dist/eim_storage -tags netgo -ldflags \
 -X eim/build.Date=${NOW}" \
 ./cmd/storage
 
+echo "Compiling eim_dispatch service..."
 go build -o dist/eim_dispatch -tags netgo -ldflags \
 "-s -w
 -X eim/build.ServiceName=EIM-Dispatch
@@ -27,6 +33,7 @@ go build -o dist/eim_dispatch -tags netgo -ldflags \
 -X eim/build.Date=${NOW}" \
 ./cmd/dispatch
 
+echo "Compiling eim_seq service..."
 go build -o dist/eim_seq -tags netgo -ldflags \
 "-s -w
 -X eim/build.ServiceName=EIM-Seq
@@ -35,6 +42,7 @@ go build -o dist/eim_seq -tags netgo -ldflags \
 -X eim/build.Date=${NOW}" \
 ./cmd/seq
 
+echo "Compiling eim_mock service..."
 go build -o dist/eim_mock -tags netgo -ldflags \
 "-s -w
 -X eim/build.ServiceName=EIM-Mock
@@ -43,8 +51,18 @@ go build -o dist/eim_mock -tags netgo -ldflags \
 -X eim/build.Date=${NOW}" \
 ./cmd/mock
 
-docker build -t eim/mock -f Dockerfile_mock ./dist
-docker build -t eim/gateway -f Dockerfile_gateway ./dist
-docker build -t eim/seq -f Dockerfile_seq ./dist
-docker build -t eim/dispatch -f Dockerfile_dispatch ./dist
-docker build -t eim/storage -f Dockerfile_storage ./dist
+echo "Compiled..."
+
+if [[ -n $1 ]] && [[ $1 == 'build_images' ]]; then
+
+  echo "Building images..."
+
+  docker build -t eim/mock -f Dockerfile_mock ./dist
+  docker build -t eim/gateway -f Dockerfile_gateway ./dist
+  docker build -t eim/seq -f Dockerfile_seq ./dist
+  docker build -t eim/dispatch -f Dockerfile_dispatch ./dist
+  docker build -t eim/storage -f Dockerfile_storage ./dist
+
+  echo "Compiled..."
+
+fi
