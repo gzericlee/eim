89bae77dcf12c4fa96df72bec7e90096ed606b39
EricLee <80889048@qq.com>
2024-07-07 00:12:09 +0800

    fix: rm swagger-ui

--- internal/api/router/init.go
+++ internal/api/router/init.go
@@ -1,11 +1,9 @@
 package router
 
 import (
-	"net/http"
+	"fmt"
 
-	restfulspec "github.com/emicklei/go-restful-openapi/v2"
 	"github.com/emicklei/go-restful/v3"
-	"github.com/go-openapi/spec"
 
 	"eim/internal/api/filter"
 	"eim/internal/redis"
@@ -15,17 +13,6 @@ import (
 func RegisterAPIRoutes(redisManager *redis.Manager) error {
 	restful.DefaultContainer.Add(regGatewayAPIs(redisManager))
 
-	config := restfulspec.Config{
-		WebServices:                   restful.RegisteredWebServices(),
-		APIPath:                       "/apidocs.json",
-		PostBuildSwaggerObjectHandler: enrichSwaggerObject,
-	}
-	restful.DefaultContainer.Add(restfulspec.NewOpenAPIService(config))
-	http.Handle("/apidocs/", http.StripPrefix("/apidocs/", http.FileServer(http.Dir("swagger-ui/dist"))))
-
-	log.Info("Get the API using /apidocs.json")
-	log.Info("Open Swagger UI using /apidocs")
-
 	cors := restful.CrossOriginResourceSharing{
 		ExposeHeaders:  []string{""},
 		AllowedHeaders: []string{"Content-Type", "Accept", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "token"},
@@ -34,19 +21,17 @@ func RegisterAPIRoutes(redisManager *redis.Manager) error {
 		Container:      restful.DefaultContainer,
 	}
 
+	wss := restful.DefaultContainer.RegisteredWebServices()
+	for _, ws := range wss {
+		log.Info(fmt.Sprintf("------------------------------> %v <------------------------------", ws.Documentation()))
+		for _, route := range ws.Routes() {
+			log.Info(route.String())
+		}
+	}
+
 	restful.Filter(cors.Filter)
 	restful.Filter(restful.OPTIONSFilter())
 	restful.Filter(filter.LogFormat())
 
 	return nil
-}
-
-func enrichSwaggerObject(swo *spec.Swagger) {
-	swo.Info = &spec.Info{
-		InfoProps: spec.InfoProps{
-			Title:       "Enterprise Instant Messaging",
-			Description: "EIM Api服务",
-			Version:     "1.0.0",
-		},
-	}
 }
